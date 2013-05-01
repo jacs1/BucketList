66e82673a446b1799c96260b5b650b5947a41c25
Joe <joseph.simoes@gmail.com>
2013-04-30 20:40:10 -0400

    Comments appending correctly to each todo item

--- Gemfile
+++ Gemfile
@@ -7,6 +7,7 @@ gem 'rails', '3.2.13'
 
 gem 'sqlite3'
 gem 'geocoder'
+gem 'pry-rails'
 
 group :test do
 	gem 'shoulda'
--- app/assets/javascripts/destinations.js
+++ app/assets/javascripts/destinations.js
@@ -1,3 +1,2 @@
-// $(document).ready(function() {
-// 	$('.toggle-comment')
-// });
+// Place all the behaviors and hooks related to the matching controller here.
+// All this logic will automatically be available in application.js.
