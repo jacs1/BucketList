59049aec531474d7d42a41c3eb57cb0e3ad5ba5a
Joe <joseph.simoes@gmail.com>
2013-05-01 17:33:54 -0400

    Destinations form

--- app/controllers/destinations_controller.rb
+++ app/controllers/destinations_controller.rb
@@ -1,6 +1,24 @@
 class DestinationsController < ApplicationController
 
 	def index
+		@destinations = Destination.all
+	end
+
+	def show
 		@destination = Destination.find(params[:id])
 	end
+
+	def create
+		@destination = Destination.new(params[:name])
+		@destination.save!
+	end
+
+
+		# render :partial => 'destinations/form', :locals => { :destination => @destination }, :content_type => 'text/html'
+
 end
+
+# def index 
+# 	items = TodoItem.near([params[:latitude], params[:longitude]])
+# 	@destinations = items.map { |i| i.destination }.uniq
+# end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Bucketlist::Application.routes.draw do
 
   resources :comments, :only => :create
   resources :todo_items, :only => :show
+  resources :destinations, :only =>[:create, :index, :show]
 
-  root :to => 'todo_items#index'
+  root :to => 'destinations#index'
 
 end
