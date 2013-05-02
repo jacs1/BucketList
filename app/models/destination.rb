ac14553e3819ec3ba6dac9c8e787e32b74f6a876
Joe <joseph.simoes@gmail.com>
2013-05-02 19:13:59 -0400

    Geocoder working

--- app/models/destination.rb
+++ app/models/destination.rb
@@ -1,4 +1,11 @@
 class Destination < ActiveRecord::Base
-  attr_accessible :name, :image_url
+  attr_accessible :name, :image_url, :latitude, :longitude
   has_many :todo_items
+
+  geocoded_by :geolocate
+  after_validation :geocode
+
+  def geolocate
+  	"#{self.name}"
+  end
 end
