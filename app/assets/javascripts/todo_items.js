17daad148c2722372a32a2d7db67d6c07987b974
Joe <joseph.simoes@gmail.com>
2013-05-03 09:57:41 -0400

    CSS Changes

--- app/assets/javascripts/todo_items.js
+++ app/assets/javascripts/todo_items.js
@@ -1,5 +1,15 @@
 $(document).ready(function() {
-    $('body').bind('ajax:success', function(e, data) {
-     $('').append(data);
- });
+
+  $('body').bind("ajax:success", function(e,data) {
+	var itemId = $('<div>').append(data).children().first().data("item-id");
+	console.log(itemId);
+		if(itemId){
+	$('.destinations' + itemId).append(data);
+}
+
+
+});
+
+
+
 });
